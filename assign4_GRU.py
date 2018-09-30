2f8251e793417a27c2043a50c3e3a8806fa501df
bklybor <bklybor@gmail.com>
2018-09-29 22:10:25 -0400

    adding other layer types

---
+++ assign4_GRU.py
@@ -0,0 +1,34 @@
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Flatten, Dropout, LSTM, GRU
+from keras.datasets import imdb
+
+batch_size = 128
+epochs = 10
+
+max_features = 500
+maxlen=50
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = Sequential()
+
+model.add(Embedding(max_features, 128))
+model.add(GRU(128, dropout = 0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print('Train...')
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
+score, acc = model.evaluate(x_test, y_test,
+                            batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
